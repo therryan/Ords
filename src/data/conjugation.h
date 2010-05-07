19279ab00ed643b82668dd826a02fb0003314412
Teemu Vasama <therryan1@gmail.com>
2010-05-07 09:04:31 +0300

    added the wrapper and conjugation class files

---
+++ src/data/conjugation.h
@@ -0,0 +1,13 @@
+// 
+// 		src/data/conjugation.h -- The Conjugation class
+// 	
+// The Conjugation class is used for words that conjugate heavily, such as irregular verbs
+
+#ifndef CONJUGATION_H
+#define CONJUGATION_H 
+
+#include "..utils/utils.h"
+
+
+
+#endif
---
+++ src/data/wrapper.h
@@ -0,0 +1,15 @@
+// 
+// 		src/data/wrapper.h -- The Wrapper class
+// 	
+// The Wrapper class can hold any definition-object, like a Definition, a DefinitionTerm or a Conjugation
+
+#ifndef WRAPPER_H
+#define WRAPPER_H
+
+#include "..utils/utils.h"
+
+#include "definition.h"
+#include "def_term.h"
+#include "conjugation.h"
+
+#endif
